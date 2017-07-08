80b4e119f9c0ffa65aecbd7959040e5ef38cc765
44kia244 <ch.kiarttipum_st@tni.ac.th>
2017-07-08 21:26:09 +0700

    Update Foundation

--- resources/assets/js/components/Layout.vue
+++ resources/assets/js/components/Layout.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
-        <div class="row">
-            <div class="columns shrink"><router-link to="/">Home</router-link></div>
-            <div class="columns shrink"><router-link to="/page1">Page1</router-link></div>
-            <div class="columns"></div>
-        </div>
-        <div class="row">
-            <div class="columns small-12 medium-12 large-12">
-                <router-view></router-view>
-            </div>
-        </div>
+    <div class="row">
+        <div class="columns">
+			<div class="row">
+				<div class="columns shrink"><router-link to="/">Home</router-link></div>
+				<div class="columns shrink"><router-link to="/page1">Page1</router-link></div>
+				<div class="columns"></div>
+			</div>
+			<div class="row">
+				<div class="columns small-12 medium-12 large-12">
+					<router-view></router-view>
+				</div>
+			</div>
+		</div>
     </div>
 </template>
 
@@ -18,3 +20,7 @@
 
     }
 </script>
+
+<style scoped>
+	
+</style>
